662574ceaca92b75136dde845d33edbe8b2db986
Ju4n1 <Juan.feuilles@gmail.com>
2024-09-13 00:37:06 -0300

    Update funciones.js

--- actividad-1-js/JS/funciones.js
+++ actividad-1-js/JS/funciones.js
@@ -3,9 +3,33 @@ function saludo(nombre){
     console.log('Hola '+nombre+" bienvenido!!!!");
 }
 
+/* Función cascarón para usar desde button Ejecutar1 */
 function saludoAux(){
-    nombre=prompt("como te llamaa");
-    saludo(nombre);
+        nombre=prompt("como te llamaa");
+        saludo(nombre);
 }
 
 
+function suma(a,b){
+
+    return (a + b);}
+
+
+
+/*Cascaron suma para button */
+function sumaAux(){
+         a=parseInt(prompt("ingrese primer numero a sumar"));
+         b=parseInt(prompt("ingrese segundo numero a sumar"));
+         c=0;
+         c=suma(a,b);
+         console.log(a+" + "+b+" = "+c);
+}
+
+
+function minuto(horas){
+        return horas*60;}
+
+function minutosaux(){
+    horas=parseInt(prompt("ingrese horas"));
+    console.log(horas+" son equivalentes a "+minuto(horas)+" minutos");
+}
