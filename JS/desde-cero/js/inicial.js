d815dad3123d58e1ba92ee0cb91e2e9a329f90a4
Ju4n1 <Juan.feuilles@gmail.com>
2025-02-06 15:43:24 -0300

    js

---
+++ JS/desde-cero/js/inicial.js
@@ -0,0 +1,21 @@
+/*
+Mensaje alerta
+alert("hola");*/
+
+
+
+/*-----------Variables------------*/
+
+
+/* Ya no se usa tanto */
+
+var producto = "Hola"; //Está incializada, podria no estarlo, el ';'es opcional, el tipo se define por el valor que toma.
+
+/*
+Definir multiples variables.
+var disponible=true,
+    tipo="computadora",
+    numero=10;*/
+
+
+console.log("Holaaaaaaaaaaa"+producto);
