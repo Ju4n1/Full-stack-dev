c3513585035ec969091b7a14f6c1107a0769dee6
Ju4n1 <Juan.feuilles@gmail.com>
2025-02-13 01:02:27 -0300

    html-desde-js

--- cafe-blog/js/scripts.js
+++ cafe-blog/js/scripts.js
@@ -1,19 +1,37 @@
 
-//Query selector.
+//-----------------Query selector.
 
 const heading=document.querySelector('.header__texto h2');//Retorna 0 o 1 elementos.
-console.log(heading);
+//console.log(heading);
 heading.textContent="Nuevo heading";
 
 const enlaces = document.querySelectorAll(".navegacion a");//De 0 a todos los que concuerden.
-console.log(enlaces[0]);
+//console.log(enlaces[0]);
 
 enlaces[0].textContent="Nuevo texto enlace";
 //enlaces[0].href="https://www.google.com";
 enlaces[0].classList.add("nueva-clase");
-enlaces[0].classList.remove("navegacion__enlace");
+//enlaces[0].classList.remove("navegacion__enlace");
 
 
 const heading2=document.getElementById("heading");
-console.log(heading2);
+//console.log(heading2);
 
+/////--------------- Crear codigo html desde JS
+
+const nuevoEnlace=document.createElement("A");
+
+//--Agrega href.
+nuevoEnlace.href="nuevo-enlace.html";
+
+//--Agrega texto.
+nuevoEnlace.textContent="Un nuevo enlase";
+
+//--Agrega clase.
+nuevoEnlace.classList.add("navegacion__enlace");
+
+//--Agregarlo al documento.
+
+const nav =document.querySelector(".navegacion");
+
+nav.appendChild(nuevoEnlace);
