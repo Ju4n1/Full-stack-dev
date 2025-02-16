6ad0467fc69b1c0a217ce9106277e0ef0bb4fc10
Ju4n1 <Juan.feuilles@gmail.com>
2025-02-16 02:18:08 -0300

    validar-formulario

--- cafe-blog/js/scripts.js
+++ cafe-blog/js/scripts.js
@@ -90,6 +90,8 @@ const datos={
 const nombre=document.querySelector("#nombre");
 const email=document.querySelector("#email");
 const mensaje=document.querySelector("#mensaje");
+const formulario=document.querySelector(".formulario");
+
 
 nombre.addEventListener("input",leerTexto);
 email.addEventListener("input",leerTexto);
@@ -97,14 +99,37 @@ mensaje.addEventListener("input",leerTexto);
 
 function leerTexto(e){
         datos[e.target.id]=e.target.value;
-        console.log(e.target.value);
-        console.log(datos);
+      //  console.log(e.target.value);
+       // console.log(datos);
 }
 
 
-const formulario=document.querySelector(".formulario");
 formulario.addEventListener("submit",function(e){
         e.preventDefault();
         //Validar formulario.
+
+        const {nombre, email, mensaje}=datos;
+        if(nombre==""||email==""||mensaje=="")
+                mostrarMensaje("Compruebe que ningún campo esté vacio.",true);
+        else
+                mostrarMensaje("Mensaje enviado correctamente");
 });
+
+//Alerta mensaje enviado
+
+function mostrarMensaje(mensaje,error=null){
+        const alerta=document.createElement("P");
+        alerta.textContent=mensaje;
+        
+        if(error)
+                alerta.classList.add("error");
+        else    alerta.classList.add("correcto");
+
+
+        formulario.appendChild(alerta);
+        // desactIvar mensaje en 5s.
+        setTimeout(()=>{
+                alerta.remove();
+        },5000);
+}
 
